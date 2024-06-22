164f95d496e847e26e348c419d0a1a8690cc26ae
kmjak <tymkoron1221@icloud.com>
2024-06-23 04:43:14 +0900

    C問題45/49で失敗

--- ABC359/C.cpp
+++ ABC359/C.cpp
@@ -9,63 +9,37 @@ int main()
     s_y++;
     t_x++;
     t_y++;
-    long long int a_x = abs(s_x - t_x);
-    long long int a_y = abs(s_y - t_y);
-    long long int m = min(a_x, a_y);
-    c = m;
-    s_x = abs(s_x) - m;
-    s_y = abs(s_y) - m;
-    if (s_x == t_x)
+    if (s_x % 2 == 0)
     {
-        c += abs(s_y);
+        if (s_y % 2 == 1)
+        {
+            s_x--;
+        }
     }
     else
     {
-        c += abs(s_x - t_x) / 2 - 0.5;
-    }
-    if (s_y % 2 == 1)
-    {
-        if (s_x < t_x)
-        {
-            c++;
+        if(s_y % 2 == 0){
+            s_x--;
         }
     }
+    long long int x = abs(s_x - t_x);
+    long long int y = abs(s_y - t_y);
+    long long int m = min(x,y);
+    if(s_x > t_x){
+        s_x -= m;
+    }else{
+        s_x += m;
+    }
+    if(s_y > t_y){
+        s_y -= m;
+    }else{
+        s_y += m;
+    }
+    c = m;
+    if(s_x == t_x){
+        c += abs(abs(t_y) - abs(s_y));
+    }else{
+        c += abs(abs(t_x) - abs(s_x)) / 2 - 0.5;
+    }
     cout << c << endl;
-
-    // while (s_x != t_x && s_y != t_y)
-    // {
-    //     if (s_y % 2 == 0)
-    //     {
-    //         if (s_x < t_x)
-    //         {
-    //             s_x++;
-    //         }
-    //     }
-    //     else
-    //     {
-    //         if (s_x > t_x)
-    //         {
-    //             s_x--;
-    //         }
-    //     }
-    //     if (s_y > t_y)
-    //     {
-    //         s_y--;
-    //         c++;
-    //     }
-    //     else
-    //     {
-    //         s_y++;
-    //         c++;
-    //     }
-    // }
-    // if (s_x == t_x)
-    // {
-    //     c += abs(s_y - t_y);
-    // }
-    // else
-    // {
-    //     c += abs(s_x - s_y) / 2 - 0.5;
-    // }
-    // cout << c << endl;
 }
