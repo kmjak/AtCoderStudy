92a90cd26d572f8d01839a3b56caffe7df356437
kmjak <tymkoron1221@icloud.com>
2024-08-14 23:05:33 +0900

    テンプレートの更新

--- template.cpp
+++ template.cpp
@@ -2,10 +2,12 @@
 #define ll long long int
 #define vi vector<int>
 #define vll vector<ll>
+#define vvi vector<vi>
+#define vvll vector<vll>
 #define vs vector<string>
 #define rep(i,s,n) for(int i = s; i < n; i++)
 #define rrep(i,s,n) for(int i = s; i >= n; i--)
-#define repauto(i, x) for(auto i : x)
+#define repa(i, x) for(auto i : x)
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 using namespace std;
