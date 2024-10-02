a0c3803d37fa93c51175ed2b071b1f57c9c9c3d8
kmjak <tymkoron1221@icloud.com>
2024-10-03 03:52:01 +0900

    EDPDのAの復讐とC,Dを解いた。D問題はわからずに解説を見ながらやった

--- EDPD/C.cpp
+++ EDPD/C.cpp
@@ -8,14 +8,13 @@
 #define rall(x) x.rbegin(), x.rend()
 #define pb push_back
 #define eb emplace_back
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define vs vector<string>
 
-using ll = long long int;
-using vi = vector<int>;
-using vll = vector<ll>;
-using vvi = vector<vi>;
-using vvll = vector<vll>;
-using vs = vector<string>;
-using vvs = vector<vs>;
 using namespace std;
 
 template<typename T> inline bool chmin(T &a, T b) {
@@ -37,5 +36,27 @@ template<typename T> inline bool chmax(T &a, T b) {
 
 int main()
 {
+  int n;
+  cin >> n;
+  vvi dp(n+1, vi(3, INT_MIN));
+
+  rep(i,0,3){
+    dp[0][i] = 0;
+  }
+
+  rep(i,0,n){
+    vi d(3);
+    cin >> d[0] >> d[1] >> d[2];
+    rep(j,0,3){
+      rep(k,0,3){
+        if(j != k) chmax(dp[i+1][j], dp[i][k] + d[k]);
+      }
+    }
+  }
+
+  int res = 0;
+  rep(i,0,3) chmax(res,dp[n][i]);
+
+  cout << res << endl;
   return 0;
 }
--- EDPD/D.cpp
+++ EDPD/D.cpp
@@ -13,6 +13,9 @@
 #define repra(i, x) for(auto i = x.rbegin(); i != x.rend(); ++i)
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define eb emplace_back
+
 using namespace std;
 
 template<typename T> inline bool chmin(T &a, T b) {
@@ -34,5 +37,35 @@ template<typename T> inline bool chmax(T &a, T b) {
 
 int main()
 {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int n,w;
+  cin >> n >> w;
+  vector<pair<ll,ll>> item;
+  item.pb(make_pair(0,0));
+  rep(i,0,n){
+    ll x,y;
+    cin >> x >> y;
+    item.pb(make_pair(x,y));
+  }
+
+  vvll dp(n+1,vll(w+1,0));
+
+  rep(i,1,n+1){
+    rep(j,0,w+1){
+      dp[i][j] = dp[i-1][j];
+      if(j >= item[i].first){
+        chmax(dp[i][j],dp[i-1][j-item[i].first] + item[i].second);
+      }
+    }
+  }
+
+  ll res = 0;
+  rep(i,1,w+1){
+    chmax(res, dp[n][i]);
+  }
+  cout << res << endl;
   return 0;
 }
