f29b41f7b48658a1f1c3b85a77db51cf1b86a83e
kmjak <tymkoron1221@icloud.com>
2024-10-11 01:41:03 +0900

    ABCの349,351,352,360のC問題を解いた

--- contest/ABC/ABC351/C.cpp
+++ contest/ABC/ABC351/C.cpp
@@ -41,31 +41,34 @@ template<typename T> inline bool chmax(T &a, T b) {
   return false;
 }
 
-
 int main()
 {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  string S,T;
-  cin >> S >> T;
-  vc tmp;
-  repa(x,S){
-    if(x == tolower(T[0]) || x == tolower(T[1]) || x == tolower(T[2])) tmp.pb(toupper(x));
-  }
-  int C = tmp.size();
-  int prev = 0;
-  if(T.back() == 'X') T = T.substr(0,2);
-  int T_size = T.size();
-  int i = 0;
-  int j = 0;
-  while(i != C){
-    if(tmp[i] == T[j]){
-      j++;
+
+  int N;
+  cin >> N;
+  vi Ball;
+  rep(i, 0, N) {
+    int x;
+    cin >> x;
+    Ball.pb(x);
+
+    while (true) {
+      int b_size = Ball.size();
+      if (b_size <= 1) {
+        break;
+      }
+      if (Ball[b_size-1] != Ball[b_size-2]) {
+        break;
+      }
+      int tmp = Ball.back();
+      Ball.pop_back();
+      Ball.pop_back();
+      Ball.pb(tmp + 1);
     }
-    i++;
   }
-  if(j == T_size) cout << "Yes" << endl;
-  else cout << "No" << endl;
+  cout << Ball.size() << endl;
   return 0;
 }
