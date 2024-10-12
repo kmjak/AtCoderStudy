ba2b6029eb0fa5ec594b65d12e4eae60c84311e5
kmjak <tymkoron1221@icloud.com>
2024-10-13 03:25:18 +0900

    ABC369のC問題を解いてみた

--- contest/ABC/ABC369/C.cpp
+++ contest/ABC/ABC369/C.cpp
@@ -1,35 +1,68 @@
 #include <bits/stdc++.h>
-#define ll long long int
-#define vi vector<int>
-#define vll vector<ll>
-#define vvi vector<vi>
-#define vvll vector<vll>
-#define vs vector<string>
-#define rep(i,s,n) for(int i = s; i < n; i++)
-#define rrep(i,s,n) for(int i = s; i >= n; i--)
+
+#define rep(i,s,n) for(auto i = s; i < n; i++)
+#define rrep(i,s,n) for(auto i = s; i > n; i--)
 #define repa(i, x) for(auto i : x)
+#define rrepa(i, x) for(auto i = x.rbegin(); i != x.rend(); ++i)
+
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    vi a(n);
-    rep(i, 0, n) cin >> a[i];
+using ll=long long int;
+using pi=pair<int,int>;
 
-    int ans = n * 2 -1;
-    int count = 0;
+using vi=vector<int>;
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
 
-    rep(i, 2, n) {
-        if (a[i] - a[i - 1] == a[i - 1] - a[i - 2]) {
-            count++;
-            ans += count;
-        } else {
-            count = 0;
-        }
-    }
+using vvi=vector<vi>;
+using vvll=vector<vll>;
 
-    cout << ans << endl;
-    return 0;
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
 }
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  ll N;
+  cin >> N;
+  vi A(N);
+  rep(i,0,N) cin >> A[i];
+
+  ll res = N;
+  ll prev = 0;
+  rep(i,1,N-1){
+    if(abs(A[i] - A[i-1]) != abs(A[i+1] - A[i])){
+      res += ((i - prev) * (i - prev + 1)) / 2;
+      prev = i;
+    }
+  }
+  res += ((N - 1 - prev) * (N - prev)) / 2;
+
+  cout << res << endl;
+  return 0;
+}
+
