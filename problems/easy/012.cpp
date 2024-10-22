ec570219cebf385c1eac961d20a6d201236bcf9c
kmjak <tymkoron1221@icloud.com>
2024-10-23 01:01:09 +0900

    AtCoder Problems easyの10-14まで終了

---
+++ problems/easy/012.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+#define rep(i,s,n) for(auto i = s; i < n; i++)
+#define rrep(i,s,n) for(auto i = s; i > n; i--)
+#define repa(i, x) for(auto i : x)
+#define rrepa(i, x) for(auto i = x.rbegin(); i != x.rend(); ++i)
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define mt make_tuple
+#define g(i,t) get<i>(t)
+#define fi first
+#define se second
+
+#define tos(n) to_string(n)
+#define toi(s) stoi(s)
+
+#define NO cout << "No" << endl;
+#define YES cout << "Yes" << endl;
+
+using namespace std;
+
+using ll=long long int;
+using pi=pair<int,int>;
+using qi=queue<int>;
+
+using vi=vector<int>;
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+
+using vvi=vector<vi>;
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int K,N;
+  cin >> K >> N;
+  vi A(N+2);
+  rep(i,0,N) {
+    cin >> A[i+1];
+  }
+  A[N+1] = A[1] + K;
+  A[0] = A[N] - K;
+  ll res = LLONG_MAX;
+  rep(i,1,N+1) {
+    ll p = K - (A[i+1] - A[i]);
+    ll m = K - (A[i] - A[i-1]);
+    chmin(res,min(p,m));
+  }
+  cout << res << '\n';
+
+  return 0;
+}
