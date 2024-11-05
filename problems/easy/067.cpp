9fb7196e5ad3ed49ac010b8a9e306518814ad41a
kmjak <tymkoron1221@icloud.com>
2024-11-06 01:55:17 +0900

    At Coder Problems easy 66-70まで終了

---
+++ problems/easy/067.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+#define rep(i,s,n) for(auto i = s; i < n; i++)
+#define rrep(i,s,n) for(auto i = s; i > n; i--)
+#define repa(i, x) for(auto i : x)
+#define rrepa(i, x) for(auto i = x.rbegin(); i != x.rend(); ++i)
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toi(s) stoi(s)
+
+#define NO cout << "No" << endl
+#define YES cout << "Yes" << endl
+
+#define vcin(N,A) rep(i,0,N) cin >> A[i]
+#define vcout(X) rep(i, 0, X.size()) cout << X[i] << (i == X.size() - 1 ? '\n' : ' ');
+
+#define sort(x) sort(all(x))
+#define rsort(x) sort(rall(x))
+
+using namespace std;
+
+using ll=long long int;
+using pi=pair<int,int>;
+using qi=queue<int>;
+
+using vi=vector<int>;
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+
+using vvi=vector<vi>;
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int N;
+  cin >> N;
+  map<string, int> Strs;
+  int MAX = 0;
+  rep(i,0,N) {
+    string tmp;
+    cin >> tmp;
+    if(Strs.find(tmp) != Strs.end()){
+      Strs[tmp]++;
+    }else{
+      Strs.insert(mp(tmp,1));
+    }
+    chmax(MAX,Strs[tmp]);
+  }
+  vs res;
+  repa(x,Strs){
+    if(x.second == MAX){
+      res.eb(x.first);
+    }
+  }
+  sort(res);
+  repa(x,res){
+    cout << x << '\n';
+  }
+  return 0;
+}
