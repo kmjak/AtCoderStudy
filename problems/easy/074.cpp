bceacb45b59d35ee72dc42b42c0ae78ea20dd982
kmjak <tymkoron1221@icloud.com>
2024-11-07 03:21:38 +0900

    At Coder Problems easy 71-75まで終了

---
+++ problems/easy/074.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+#define rep(i,s,n) for(auto i = s; i < n; i++)
+#define rrep(i,s,n) for(auto i = s; i > n; i--)
+#define repa(i, x) for(auto i : x)
+#define rrepa(i, x) for(auto i = x.rbegin(); i != x.rend(); ++i)
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toi(s) stoi(s)
+
+#define NO cout << "No" << endl
+#define YES cout << "Yes" << endl
+
+#define vcin(N,A) rep(i,0,N) cin >> A[i]
+#define vcout(X) rep(i, 0, X.size()) cout << X[i] << (i == X.size() - 1 ? '\n' : ' ');
+
+using namespace std;
+
+using ll=long long int;
+using pi=pair<int,int>;
+using qi=queue<int>;
+using si=set<int>;
+
+using vi=vector<int>;
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+
+using vvi=vector<vi>;
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int a,b,c,x,y;
+  cin >> a >> b >> c >> x >> y;
+  ll res = 0;
+  c *= 2;
+  if(c <= a + b){
+    int m = min(x,y);
+    x -= m;
+    y -= m;
+    res += m * c;
+    if(x && a > c){
+      res += x * c;
+      x = 0;
+    }else if(y && b > c){
+      res += y * c;
+      y = 0;
+    }
+  }
+  if(x){
+    res += a * x;
+  }
+  if(y){
+    res += b * y;
+  }
+  cout << res << '\n';
+  return 0;
+}
