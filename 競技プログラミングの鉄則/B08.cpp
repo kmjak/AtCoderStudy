a059f3364332f0cac85c3d3781aa7ef099f95350
kmjak <tymkoron1221@icloud.com>
2025-01-09 17:17:20 +0900

    B08を解いた

---
+++ 競技プログラミングの鉄則/B08.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+#define rep(i,s,n) for(auto i = s; i < n; i++)
+#define rrep(i,s,n) for(auto i = s; i > n; i--)
+#define repa(i, x) for(auto i : x)
+#define rrepa(i, x) for(auto i = x.rbegin(); i != x.rend(); ++i)
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toi(s) stoi(s)
+#define tob(b) static_cast<int>(b.to_ulong())
+
+#define NO cout << "No" << endl
+#define YES cout << "Yes" << endl
+#define END cout << '\n'
+
+#define vcin(N,A) rep(i,0,N) cin >> A[i]
+#define vcout(X) rep(i, 0, X.size()) cout << X[i] << (i == X.size() - 1 ? '\n' : ' ');
+
+using namespace std;
+
+using ll=long long int;
+using pi=pair<int,int>;
+using qi=queue<int>;
+using qp=queue<pi>;
+using si=set<int>;
+
+using vi=vector<int>;
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvi=vector<vi>;
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+struct pnt {
+  ll x;
+  ll y;
+};
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int N;
+  cin >> N;
+  int MAX =1500;
+  vvi field(MAX+10,vi(MAX+10,0));
+  rep(i,0,N) {
+    int x,y;
+    cin >> x >> y;
+    field[y][x]++;
+  }
+  rep(i,0,MAX+5) {
+    rep(j,0,MAX+5) {
+      field[i][j+1] += field[i][j];
+    }
+  }
+  int Q;
+  cin >> Q;
+  while(Q--){
+    pnt s,g;
+    cin >> s.x >> s.y >> g.x >> g.y;
+    int sum = 0;
+    rep(i,s.y, g.y+1) {
+      sum += field[i][g.x] - field[i][s.x-1];
+    }
+    cout << sum << '\n';
+  }
+  return 0;
+}
