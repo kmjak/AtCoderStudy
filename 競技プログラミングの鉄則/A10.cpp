fc7e55d25b21d537ad5a5a0fa3e4db1f27f76fd6
kmjak <tymkoron1221@icloud.com>
2025-01-18 17:37:35 +0900

    11まで終了

---
+++ 競技プログラミングの鉄則/A10.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+#define rep(i,s,n) for(auto i = s; i < n; i++)
+#define rrep(i,s,n) for(auto i = s; i > n; i--)
+#define repa(i, x) for(auto i : x)
+#define rrepa(i, x) for(auto i = x.rbegin(); i != x.rend(); ++i)
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toi(s) stoi(s)
+#define btoi(b) static_cast<int>(b.to_ulong())
+
+#define NO cout << "No" << endl
+#define YES cout << "Yes" << endl
+#define END cout << '\n'
+
+#define vcin(N,A) rep(i,0,N) cin >> A[i]
+#define vcout(X) rep(i, 0, X.size()) cout << X[i] << (i == X.size() - 1 ? '\n' : ' ');
+
+using namespace std;
+
+using ll=long long int;
+using pi=pair<int,int>;
+using qi=queue<int>;
+using qp=queue<pi>;
+using si=set<int>;
+
+using vi=vector<int>;
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvi=vector<vi>;
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+struct pnt {
+  ll x;
+  ll y;
+};
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int N,D;
+  cin >> N;
+  vi A(N),left(N+1,0),right(N+1,0);
+  rep(i,0,N) {
+    cin >> A[i];
+  }
+  rep(i,0,N) {
+    left[i+1] += max(left[i], A[i]);
+    right[i+1] = max(right[i],A[N-i-1]);
+  }
+  cin >> D;
+  while(D--){
+    int l,r;
+    cin >> l >> r;
+    l--;r--;
+    cout << max(left[l],right[N-r-1]) << '\n';
+  }
+  // vcout(left);
+  // vcout(right);
+  return 0;
+}
