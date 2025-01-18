a2a514482b4913ac46bc386e8100b0158705ec8e
kmjak <tymkoron1221@icloud.com>
2025-01-18 22:44:43 +0900

    C問題で累積和が使えることに気付くのに時間がかかった。D問題はTLEになってしまって最適をしていたけどうまくいかなかった

---
+++ contest/ABC/ABC389/D.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+#define rep(i,s,n) for(auto i = s; i < n; i++)
+#define rrep(i,s,n) for(auto i = s; i > n; i--)
+#define repa(i, x) for(auto i : x)
+#define rrepa(i, x) for(auto i = x.rbegin(); i != x.rend(); ++i)
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toi(s) stoi(s)
+#define btoi(b) static_cast<int>(b.to_ulong())
+
+#define NO cout << "No" << endl
+#define YES cout << "Yes" << endl
+
+#define vcin(A) rep(i,0,A.size()) cin >> A[i]
+#define vcout(X) rep(i, 0, X.size()) cout << X[i] << (i == X.size() - 1 ? '\n' : ' ')
+
+#define bs(A,X) binary_search(all(A),X)
+#define lbs(A,X) lower_bound(all(A),X)
+#define ubs(A,X) upper_bound(all(A),X)
+
+using namespace std;
+
+using ll=long long int;
+using pi=pair<int,int>;
+using qi=queue<int>;
+using qp=queue<pi>;
+using si=set<int>;
+
+using vi=vector<int>;
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvi=vector<vi>;
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+struct pnt {
+  ll x;
+  ll y;
+};
+
+int R;
+
+ll sum(int n)
+{
+  if(n < 3) return 0;
+  return ((n-2)*(n-1))/2;
+}
+
+bool calc(double x, double y)
+{
+  x+=0.5;
+  y+=0.5;
+  // cout << sqrt(x*x + y*y) << '\n';
+  return sqrt(x*x + y*y) < R;
+}
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  cin >> R;
+  ll res = (R-1)*4+1+sum(R)*4;
+  rep(i,1,R+1) {
+    rep(j,R-i,R+1) {
+      if(calc(i,j)){
+        res += 4;
+      }else{
+        break;
+      }
+    }
+  }
+
+  cout << res << '\n';
+}
