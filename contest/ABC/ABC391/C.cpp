b12a4b9cba9a16ef7f42e3dc78e4d5e59d3c77f1
kmjak <tymkoron1221@icloud.com>
2025-02-01 22:48:09 +0900

    C問題が解けた時絶対+になった自信あったけど-だった

---
+++ contest/ABC/ABC391/C.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+
+#define rep(i,s,n) for(auto i = s; i < n; i++)
+#define rrep(i,s,n) for(auto i = s; i > n; i--)
+#define repa(i, x) for(auto i : x)
+#define rrepa(i, x) for(auto i = x.rbegin(); i != x.rend(); ++i)
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toi(s) stoi(s)
+#define btoi(b) static_cast<int>(b.to_ulong())
+
+#define NO cout << "No" << endl
+#define YES cout << "Yes" << endl
+
+#define vcin(A) rep(i,0,A.size()) cin >> A[i]
+#define vcout(X) rep(i, 0, X.size()) cout << X[i] << (i == X.size() - 1 ? '\n' : ' ')
+
+#define bs(A,X) binary_search(all(A),X)
+#define lbs(A,X) lower_bound(all(A),X)
+#define ubs(A,X) upper_bound(all(A),X)
+
+using namespace std;
+
+using ll=long long int;
+using pi=pair<int,int>;
+using qi=queue<int>;
+using qp=queue<pi>;
+using si=set<int>;
+
+using vi=vector<int>;
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvi=vector<vi>;
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+struct pnt {
+  ll x;
+  ll y;
+};
+
+int N,Q;
+vi H(1e6+10,1),P(1e6+10);
+map<int, int> moved;
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  cin >> N >> Q;
+  rep(i,1,N+1)P[i]=i;
+  int res=0;
+  while(Q--){
+    int opt;
+    cin >> opt;
+    // cout << "opt: " << opt << endl;
+    if(opt == 1){
+      int p,h;
+      cin >> p >> h;
+      int prev = P[p];
+      moved[prev]--;
+      moved[h]++;
+      H[h]++;
+      H[prev]--;
+      P[p] = h;
+    }else{
+      while(!moved.empty()){
+        auto [idx, x] = *moved.begin();
+        // cout << "idx: " << idx << " x: " << x << endl;
+        if(x==0){
+          moved.erase(idx);
+          continue;
+        };
+        if(x>0){
+          if(H[idx]>1&&H[idx]-x<2){
+            // cout<<idx<<"add"<<endl;
+            res++;
+          }
+        }
+        if(x<0){
+          if(H[idx]<2&&H[idx]-x>1){
+            // cout<<idx<<"sub"<<endl;
+            res--;
+          }
+        }
+        moved.erase(idx);
+      }
+      cout << res << endl;
+    }
+  }
+  // rep(i,0,N+1){
+  //   cout << "H[" << i << "]: " << H[i] << endl;
+  // }
+  return 0;
+}
