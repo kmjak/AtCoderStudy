f8287e49f534dee959ebac5af3ec94b089d3010d
kmjak <tymkoron1221@icloud.com>
2025-02-22 22:44:08 +0900

    Dが下振れの日だったのでEも解きたかったけど文字の復元がいまいちうまくできなかった

---
+++ contest/ABC/ABC394/E.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+
+#define rep(i,s,n) for(auto i = s; i < n; i++)
+#define rrep(i,s,n) for(auto i = s; i > n; i--)
+#define repa(i, x) for(auto i : x)
+#define rrepa(i, x) for(auto i = x.rbegin(); i != x.rend(); ++i)
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toi(s) stoi(s)
+#define btoi(b) static_cast<int>(b.to_ulong())
+
+#define NO cout << "No" << endl
+#define YES cout << "Yes" << endl
+
+#define vcin(A) rep(i,0,A.size()) cin >> A[i]
+#define vcout(X) rep(i, 0, X.size()) cout << X[i] << (i == X.size() - 1 ? '\n' : ' ')
+
+#define bs(A,X) binary_search(all(A),X)
+#define lbs(A,X) lower_bound(all(A),X)
+#define ubs(A,X) upper_bound(all(A),X)
+
+using namespace std;
+
+using ll=long long int;
+using pi=pair<int,int>;
+using qi=queue<int>;
+using qp=queue<pi>;
+using si=set<int>;
+
+using vi=vector<int>;
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvi=vector<vi>;
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+struct pnt {
+  ll from;
+  ll to;
+};
+
+vvi G(110);
+vector<vector<string>> label(110,vector<string>(110));
+int N;
+
+vector<string> bfs(int s,int g)
+{
+  vi dist(N,-1);
+  vector<string> res;
+  map<int,vector<int>> memo;
+  qi q;
+  q.push(s);
+  dist[s]=0;
+  while(!q.empty()){
+    int now=q.front();
+    q.pop();
+    repa(next,G[now]){
+      memo[next].eb(now);
+      if(dist[next]==-1){
+        dist[next]=dist[now]+1;
+        q.push(next);
+      }
+    }
+  }
+  if(dist[g]==-1){
+    return res;
+  }
+  // sからgまでで作れる全文字を復元したかった
+  string tmp=label[memo[g][0]][g];
+  memo[g].erase(memo[g].begin());
+  cout<<tmp<<endl;
+  return res;
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  cin >> N;
+  rep(i,0,N){
+    string s;
+    cin >> s;
+    rep(j,0,N){
+      string tmp=s.substr(j,1);
+      label[i][j]=tmp;
+      if(label[i][j]!="-"){
+        G[i].eb(j);
+      }
+    }
+  }
+  rep(i,0,N){
+    rep(j,0,N){
+      if(i==j){
+        // cout<<0<<" ";
+        continue;
+      }
+      bfs(i,j);
+    }
+    cout<<endl;
+  }
+  return 0;
+}
