10526678e0e7b01981794a334f445697de31612e
kmjak <tymkoron1221@icloud.com>
2025-03-30 08:36:00 +0900

    閉路問題はUnion Findを使ったら便利と知った

--- contest/ABC/ABC399/C.cpp
+++ contest/ABC/ABC399/C.cpp
@@ -51,6 +51,79 @@ template<typename T> inline bool chmax(T &a, T b) {
   return false;
 }
 
+/// @brief Union-Find 木
+/// @note 1.4 高速化 + 省メモリ化
+/// @see https://zenn.dev/reputeless/books/standard-cpp-for-competitive-programming/viewer/union-find
+class UnionFind
+{
+public:
+
+	UnionFind() = default;
+
+	/// @brief Union-Find 木を構築します。
+	/// @param n 要素数
+	explicit UnionFind(size_t n)
+		: m_parentsOrSize(n, -1) {}
+
+	/// @brief 頂点 i の root のインデックスを返します。
+	/// @param i 調べる頂点のインデックス
+	/// @return 頂点 i の root のインデックス
+	int find(int i)
+	{
+		if (m_parentsOrSize[i] < 0)
+		{
+			return i;
+		}
+
+		// 経路圧縮
+		return (m_parentsOrSize[i] = find(m_parentsOrSize[i]));
+	}
+
+	/// @brief a のグループと b のグループを統合します。
+	/// @param a 一方のインデックス
+	/// @param b 他方のインデックス
+	void merge(int a, int b)
+	{
+		a = find(a);
+		b = find(b);
+
+		if (a != b)
+		{
+			// union by size (小さいほうが子になる）
+			if (-m_parentsOrSize[a] < -m_parentsOrSize[b])
+			{
+				std::swap(a, b);
+			}
+
+			m_parentsOrSize[a] += m_parentsOrSize[b];
+			m_parentsOrSize[b] = a;
+		}
+	}
+
+	/// @brief a と b が同じグループに属すかを返します。
+	/// @param a 一方のインデックス
+	/// @param b 他方のインデックス
+	/// @return a と b が同じグループに属す場合 true, それ以外の場合は false
+	bool connected(int a, int b)
+	{
+		return (find(a) == find(b));
+	}
+
+	/// @brief i が属するグループの要素数を返します。
+	/// @param i インデックス
+	/// @return i が属するグループの要素数
+	int size(int i)
+	{
+		return -m_parentsOrSize[find(i)];
+	}
+
+private:
+
+	// m_parentsOrSize[i] は i の 親,
+	// ただし root の場合は (-1 * そのグループに属する要素数)
+	std::vector<int> m_parentsOrSize;
+};
+
 struct pnt {
   ll x;
   ll y;
@@ -59,51 +132,18 @@ struct pnt {
 int N,M;
 vvi G(2e5+10);
 vb visited(2e5+10, false);
-int res=0;
 
-void bfs(int st, int &n, int &k) {
-  queue<int> q;
-  q.push(st);
-  visited[st] = true;
-
-  while (!q.empty()) {
-    int u = q.front();
-    q.pop();
-    n++;
-
-    for (int v : G[u]) {
-      k++;
-      if (!visited[v]) {
-        visited[v] = true;
-        q.push(v);
-      }
-    }
-  }
-}
-
-int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-
+int main()
+{
+  int N, M;
   cin >> N >> M;
-
-  for (int i = 0; i < M; ++i) {
+  UnionFind uf(N);
+  int K = N;
+  for (int i = 0; i < M; i++) {
     int u, v;
     cin >> u >> v;
-    G[u].push_back(v);
-    G[v].push_back(u);
+    --u, --v;
+    if (!uf.connected(u, v)) uf.merge(u, v), K--;
   }
-
-  for (int i = 1; i <= N; ++i) {
-    if (!visited[i]) {
-      int n=0;
-      int k=0;
-      bfs(i, n, k);
-      k/=2;
-      res+=k-(n-1);
-    }
-  }
-
-  cout << res << endl;
-  return 0;
+  cout << M - (N - K) << "\n";
 }
