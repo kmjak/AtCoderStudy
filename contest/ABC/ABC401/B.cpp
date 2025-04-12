4a31db7caa2e6171969668d1407c3a70c0093e0a
kmjak <tymkoron1221@icloud.com>
2025-04-13 02:37:58 +0900

    ABC401: D問題でDFSを使えば解けそうと思ったけど時間切れ

---
+++ contest/ABC/ABC401/B.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toi(s) stoi(s)
+#define btoi(b) static_cast<int>(b.to_ulong())
+
+#define bs(A,X) binary_search(all(A),X)
+#define lbs(A,X) lower_bound(all(A),X)
+#define ubs(A,X) upper_bound(all(A),X)
+
+using namespace std;
+
+using ll=long long int;
+using pi=pair<int,int>;
+using qi=queue<int>;
+using qp=queue<pi>;
+using si=set<int>;
+
+using vi=vector<int>;
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvi=vector<vi>;
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+struct pnt {
+  ll x;
+  ll y;
+};
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int N;
+  cin>>N;
+  bool isLogin=false;
+  int res=0;
+  while(N--){
+    string s;
+    cin>>s;
+    if(s=="login"){
+      isLogin=true;
+    }else if(s=="logout"){
+      isLogin=false;
+    }else if(s=="private"){
+      if(!isLogin){
+        res++;
+      }
+    }
+  }
+  cout<<res<<endl;
+  return 0;
+}
