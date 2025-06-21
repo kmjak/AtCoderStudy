a35e70b12f08def88262358ab3efb12705eb694d
kmjak <tymkoron1221@icloud.com>
2025-06-21 21:25:22 +0900

    feat: 用事があるので10分間で終了

---
+++ contest/ABC/ABC411/B.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toi(s) stoi(s)
+#define btoi(b) static_cast<int>(b.to_ulong())
+
+using namespace std;
+
+using ll=long long int;
+using pi=pair<int,int>;
+using qi=queue<int>;
+using qp=queue<pi>;
+using si=set<int>;
+
+using vi=vector<int>;
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvi=vector<vi>;
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+struct pnt {
+  ll x;
+  ll y;
+};
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int N;
+  cin>>N;
+  vi D(N-1);
+  for(int i=0;i<N-1;i++)cin>>D[i];
+  for(int i=0;i<N-1;i++){
+    int sum=0;
+    for(int j=i;j<N-1;j++){
+      sum+=D[j];
+      cout << sum << (j+2 == N ? "\n":" ");
+    }
+  }
+
+  return 0;
+}
