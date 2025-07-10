538d97dc3004fdf3cf3192e778269c7cd5542d1e
kmjak <tymkoron1221@icloud.com>
2025-07-10 23:49:21 +0900

    tenkei027: setにinsertに成功したらi+1を出力する方法で解いた

---
+++ tenkei/027.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toi(s) stoi(s)
+#define btoi(b) static_cast<ll>(b.to_ulong())
+
+using namespace std;
+
+using ll=long long int;
+using pll=pair<ll,ll>;
+using qll=queue<ll>;
+using qp=queue<pll>;
+using sll=set<ll>;
+
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+struct pnt {
+  ll x;
+  ll y;
+};
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  ll N;cin>>N;
+  set<string> se;
+  for(ll i=0;i<N;i++){
+    string s;cin>>s;
+    if(se.insert(s).second){
+      cout<<i+1<<endl;
+    }
+  }
+  return 0;
+}
