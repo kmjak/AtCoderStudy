f89e3fcc8921eb72704f1134e572f0ff6b8fb5f0
kmjak <tymkoron1221@icloud.com>
2025-07-11 00:33:14 +0900

    tenkei038: A*(B/n)とするとオーバーフローを起こすので、1e18/(B/n)とすることで対処した

---
+++ tenkei/038.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toi(s) stoi(s)
+#define btoi(b) static_cast<ll>(b.to_ulong())
+
+using namespace std;
+
+using ll=long long int;
+using pll=pair<ll,ll>;
+using qll=queue<ll>;
+using qp=queue<pll>;
+using sll=set<ll>;
+
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+struct pnt {
+  ll x;
+  ll y;
+};
+
+ll gcd(ll a, ll b){
+  if(b==0)return a;
+  return gcd(b,a%b);
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  ll A,B;cin>>A>>B;
+  ll n=gcd(A,B);
+  ll m=B/n;
+  cout<<(A<=(long double)1e18/m?to_string(A*m):"Large")<<endl;
+  return 0;
+}
