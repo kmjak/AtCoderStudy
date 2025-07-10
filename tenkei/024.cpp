9f0c4cbed46fad868c7ce8c45032d76a35974a7d
kmjak <tymkoron1221@icloud.com>
2025-07-10 23:44:40 +0900

    renkei024: 全体の誤差を求めて、誤差がK以下かつKと誤差を2で割ったあまりが同じであればYesそれ以外ならNoで解いた

---
+++ tenkei/024.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toi(s) stoi(s)
+#define btoi(b) static_cast<ll>(b.to_ulong())
+
+using namespace std;
+
+using ll=long long int;
+using pll=pair<ll,ll>;
+using qll=queue<ll>;
+using qp=queue<pll>;
+using sll=set<ll>;
+
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+struct pnt {
+  ll x;
+  ll y;
+};
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  ll N,K;
+  cin >> N >> K;
+  vll A(N), B(N);
+  for(ll i=0;i<N;i++)cin>>A[i];
+  for(ll i=0;i<N;i++)cin>>B[i];
+
+  ll totalDiff=0;
+  for(ll i=0;i<N;i++)totalDiff+=abs(A[i]-B[i]);
+  if(totalDiff<=K){
+    cout<<(totalDiff%2==K%2 ? "Yes" : "No")<<endl;
+  }else{
+    cout<<"No"<<endl;
+  }
+  return 0;
+}
