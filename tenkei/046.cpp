63d445abd982e9c97773b7b4ddb29fac236e6640
kmjak <tymkoron1221@icloud.com>
2025-07-12 02:40:19 +0900

    tenkei046: A,B,Cそれぞれデータを取得するタイミングで46で割ったあまりで値を保管することで、46^3で計算できる

---
+++ tenkei/046.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toi(s) stoi(s)
+#define btoi(b) static_cast<ll>(b.to_ulong())
+
+using namespace std;
+
+using ll=long long int;
+using pll=pair<ll,ll>;
+using qll=queue<ll>;
+using qp=queue<pll>;
+using sll=set<ll>;
+
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+vll dx={1, 0, -1, 0};
+vll dy={0, 1, 0, -1};
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  ll N;cin >> N;
+  map<ll,ll> A, B, C;
+  for(ll i = 0; i < N; i++){
+    ll a;cin >> a;
+    A[a%46]++;
+  }
+  for(ll i = 0; i < N; i++){
+    ll b;cin >> b;
+    B[b%46]++;
+  }
+  for(ll i = 0; i < N; i++){
+    ll c;cin >> c;
+    C[c%46]++;
+  }
+
+  ll res=0;
+  for(auto a: A){
+    for(auto b: B){
+      for(auto c: C){
+        if((a.first + b.first + c.first) % 46 == 0){
+          res += a.second * b.second * c.second;
+        }
+      }
+    }
+  }
+
+  cout << res << endl;
+
+  return 0;
+}
