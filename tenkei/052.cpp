0e2dead4020ee5bd2d3aefb9bb3ee5f68b70a1aa
kmjak <tymkoron1221@icloud.com>
2025-07-12 18:20:50 +0900

    tenkei052: サイコロの数を合計して、書く合計をかけて解いた

---
+++ tenkei/052.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toi(s) stoi(s)
+#define btoi(b) static_cast<ll>(b.to_ulong())
+
+using namespace std;
+
+using ll=long long int;
+using pll=pair<ll,ll>;
+using qll=queue<ll>;
+using qp=queue<pll>;
+using sll=set<ll>;
+
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+vll dx={1, 0, -1, 0};
+vll dy={0, 1, 0, -1};
+const ll MOD=1e9+7;
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  ll N;
+  cin>> N;
+  vll A(N);
+  for(ll i=0;i<N;i++){
+    ll score=0;
+    for(ll j=0;j<6;j++){
+      ll x;
+      cin>> x;
+      score+=x;
+    }
+    A[i]=score;
+  }
+
+  ll res=1;
+  for(ll i=0;i<N;i++){
+    res=(res*A[i])%MOD;
+  }
+  cout<< res << endl;
+
+
+  return 0;
+}
