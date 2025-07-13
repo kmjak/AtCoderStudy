f4960c7b7d79ec7801608ffdaff42b390443269c
kmjak <tymkoron1221@icloud.com>
2025-07-14 03:44:34 +0900

    tenkei067: Mが0の時にsが空文字を出力する可能性があることを考慮し忘れていた

---
+++ tenkei/067.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toi(s) stoi(s)
+#define btoi(b) static_cast<ll>(b.to_ulong())
+
+using namespace std;
+
+using ll=long long int;
+using qll=queue<ll>;
+using dq=deque<ll>;
+
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+vll dx={1, 0, -1, 0};
+vll dy={0, 1, 0, -1};
+const ll MOD=1e9+7;
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  string N;
+  ll K;
+  cin>>N>>K;
+  while(K--){
+    ll M=0;
+    for (ll i=0;i<N.size();i++)M=M*8+ll(N[i]-'0');
+    string s="";
+    while (M) {
+      s+=toc(M%9);
+      M/=9;
+    }
+    if(s==""){
+      break;
+    }
+    reverse(all(s));
+    replace(all(s),'8','5');
+    N=s;
+  }
+  cout<<N<<endl;
+  return 0;
+}
