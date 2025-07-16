f11b74cd5f55c8b198d0d2f97d7a1c70357b5594
kmjak <tymkoron1221@icloud.com>
2025-07-17 02:16:19 +0900

    tenkei078: グラフの問題に見えてaとbの大きい方にインクリメントして、最終的に1..Nの中でcntがちょうど1の数を数えるだけで良かった

---
+++ tenkei/078.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toll(s) stoll(s)
+#define btoi(b) static_cast<ll>(b.to_ulong())
+
+using namespace std;
+
+using ll=long long int;
+using qll=queue<ll>;
+using dq=deque<ll>;
+
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+vll dx={1, 0, -1, 0};
+vll dy={0, 1, 0, -1};
+const ll MOD=1e9+7;
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  ll N,M;cin>>N>>M;
+  vll G(N);
+  for(ll i=0;i<M;i++){
+    ll a,b;cin>>a>>b;
+    a--;b--;
+    G[max(a,b)]++;
+  }
+  ll res=0;
+  for(ll i=0;i<N;i++)res+=(G[i]==1);
+  cout<<res<<endl;
+  return 0;
+}
