bf5740f7534d2dca31b6e02290fe407540d6a841
kmjak <tymkoron1221@icloud.com>
2025-07-27 01:11:47 +0900

    ABC416: コンテストには参加できなかったけど問題を解いた

---
+++ contest/ABC/ABC416/C.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define toll(s) stoll(s)
+#define btoi(b) static_cast<ll>(b.to_ulong())
+
+using namespace std;
+
+using ll=long long int;
+using qll=queue<ll>;
+using dq=deque<ll>;
+
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+class SegmentTreeRMQ {
+  public:
+  vll data;
+  ll size = 1;
+
+  void init(ll n){
+    while(size < n) size *= 2;
+    data.assign(size * 2, 0);
+  }
+
+  void update(ll pos, ll x){
+    pos += size - 1;
+    data[pos] = x;
+    while(pos >= 2){
+      pos /= 2;
+      data[pos] = max(data[pos*2], data[pos*2+1]);
+    }
+  }
+
+  ll query(ll l, ll r, ll bl, ll br, ll u){
+    if(r <= bl || br <= l)return -1e18;
+    if(l <= bl && br <= r)return data[u];
+    ll m = (bl + br) / 2;
+    ll al = query(l, r, bl, m, u*2);
+    ll ar= query(l, r, m, br, u*2+1);
+    return max(al, ar);
+  }
+};
+
+class SegmentTreeRSQ {
+  public:
+  vll data;
+  ll size = 1;
+
+  void init(ll n){
+    while(size < n) size *= 2;
+    data.assign(size * 2, 0);
+  }
+
+  void update(ll pos, ll x){
+    pos += size - 1;
+    data[pos] = x;
+    while(pos >= 2){
+      pos /= 2;
+      data[pos] = data[pos*2] + data[pos*2+1];
+    }
+  }
+
+  ll query(ll l, ll r, ll bl, ll br, ll u){
+    if(r <= bl || br <= l)return 0;
+    if(l <= bl && br <= r)return data[u];
+    ll m = (bl + br) / 2;
+    ll al = query(l, r, bl, m, u*2);
+    ll ar= query(l, r, m, br, u*2+1);
+    return al + ar;
+  }
+};
+
+bool isRange(ll x, ll y, ll w, ll h){
+  return (0 <= x && x < w) && (0 <= y && y < h);
+}
+
+vll dx={1, 0, -1, 0};
+vll dy={0, 1, 0, -1};
+const ll MOD=1e9+7;
+vs res;
+ll N,K,X;
+
+void dfs(ll cur, ll k, string r, vs A){
+  r+= A[cur];
+  if(k==0){
+    res.eb(r);
+    return;
+  }
+  for(ll i=0;i<N;i++){
+    dfs(i,k-1,r,A);
+  }
+}
+
+/**
+ * 考察
+ * N<=10
+ * K<=5
+ * なのでdfsで全探索して、X番目に小さい値を出力する
+ */
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  cin>>N>>K>>X;
+  vs A(N);
+  for(ll i=0;i<N;i++)cin>>A[i];
+
+  sort(all(A));
+
+  for(ll i=0;i<N;i++){
+    dfs(i,K-1,"",A);
+  }
+  sort(all(res));
+  // cout<<res.size()<<endl;
+  cout<<res[X-1]<<endl;
+  return 0;
+}
