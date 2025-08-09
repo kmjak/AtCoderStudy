183822bc5563e694b65ba711542cfb719ba639c6
kmjak <tymkoron1221@icloud.com>
2025-08-09 14:07:10 +0900

    ABC387: D問題を解いた

--- contest/ABC/ABC387/D.cpp
+++ contest/ABC/ABC387/D.cpp
@@ -1,45 +1,29 @@
 #include <bits/stdc++.h>
 
-#define rep(i,s,n) for(auto i = s; i < n; i++)
-#define rrep(i,s,n) for(auto i = s; i > n; i--)
-#define repa(i, x) for(auto i : x)
-#define rrepa(i, x) for(auto i = x.rbegin(); i != x.rend(); ++i)
-
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 
 #define mp make_pair
 #define mt make_tuple
-#define pb push_back
 #define eb emplace_back
 #define g(i,t) get<i>(t)
 
 #define tos(n) to_string(n)
 #define toc(n) '0' + n
-#define toi(s) stoi(s)
-
-#define NO cout << "No" << endl
-#define YES cout << "Yes" << endl
-#define END cout << '\n'
-
-#define vcin(N,A) rep(i,0,N) cin >> A[i]
-#define vcout(X) rep(i, 0, X.size()) cout << X[i] << (i == X.size() - 1 ? '\n' : ' ');
+#define toll(s) stoll(s)
+#define btoi(b) static_cast<ll>(b.to_ulong())
 
 using namespace std;
 
 using ll=long long int;
-using pi=pair<int,int>;
-using qi=queue<int>;
-using qp=queue<pi>;
-using si=set<int>;
+using qll=queue<ll>;
+using dq=deque<ll>;
 
-using vi=vector<int>;
 using vll=vector<ll>;
 using vs=vector<string>;
 using vc=vector<char>;
 using vb=vector<bool>;
 
-using vvi=vector<vi>;
 using vvll=vector<vll>;
 
 template<typename T> inline bool chmin(T &a, T b) {
@@ -58,91 +42,141 @@ template<typename T> inline bool chmax(T &a, T b) {
   return false;
 }
 
-struct pnt {
-  ll x;
-  ll y;
-  char prev_action;
+class SegmentTreeRMQ {
+  public:
+  vll data;
+  ll size = 1;
+
+  void init(ll n){
+    while(size < n) size *= 2;
+    data.assign(size * 2, 0);
+  }
+
+  void update(ll pos, ll x){
+    pos += size - 1;
+    data[pos] = x;
+    while(pos >= 2){
+      pos /= 2;
+      data[pos] = max(data[pos*2], data[pos*2+1]);
+    }
+  }
+
+  ll query(ll l, ll r, ll bl, ll br, ll u){
+    if(r <= bl || br <= l)return -1e18;
+    if(l <= bl && br <= r)return data[u];
+    ll m = (bl + br) / 2;
+    ll al = query(l, r, bl, m, u*2);
+    ll ar= query(l, r, m, br, u*2+1);
+    return max(al, ar);
+  }
 };
 
-ll H,W;
-vi dx = {1,0,-1,0};
-vi dy = {0,1,0,-1};
+class SegmentTreeRSQ {
+  public:
+  vll data;
+  ll size = 1;
 
-int bfs(vs &field, int sx, int sy)
-{
-  queue<pnt> que;
-  vvi dist(H, vi(W, -1));
-  que.push(pnt{sx, sy, 'S'});
-  dist[sx][sy] = 0;
-  while(!que.empty()) {
-    pnt p = que.front();
-    que.pop();
-    rep(i,0,4) {
-      if(p.prev_action == 'X' && i % 2 == 0) {
-        continue;
+  void init(ll n){
+    while(size < n) size *= 2;
+    data.assign(size * 2, 0);
+  }
+
+  void update(ll pos, ll x){
+    pos += size - 1;
+    data[pos] = x;
+    while(pos >= 2){
+      pos /= 2;
+      data[pos] = data[pos*2] + data[pos*2+1];
+    }
+  }
+
+  ll query(ll l, ll r, ll bl, ll br, ll u){
+    if(r <= bl || br <= l)return 0;
+    if(l <= bl && br <= r)return data[u];
+    ll m = (bl + br) / 2;
+    ll al = query(l, r, bl, m, u*2);
+    ll ar= query(l, r, m, br, u*2+1);
+    return al + ar;
+  }
+};
+
+bool isRange(ll x, ll y, ll w, ll h){
+  return (0 <= x && x < w) && (0 <= y && y < h);
+}
+
+vll dx={1, 0, -1, 0};
+vll dy={0, 1, 0, -1};
+const ll MOD=1e9+7;
+
+/**
+ * 考察
+ *
+ */
+
+struct que{
+  pair<ll,ll> pos;
+  ll sc;
+  ll p;
+};
+
+ll H, W;
+vs G(1010);
+pair<ll,ll> st,gl;
+
+ll bfs(){
+  queue<que> q;
+  vvll distP0(H,vll(W,-1));
+  vvll distP1(H,vll(W,-1));
+  distP0[st.first][st.second]=0;
+  q.push({st, 0, 0});
+  for(ll i=-1;i<2;i+=2){
+    ll nx = st.second + i;
+    ll ny = st.first;
+    if(!isRange(nx, ny, W, H))continue;
+    if(G[ny][nx]=='#')continue;
+    q.push({mp(ny, nx), 0, 1});
+    distP1[ny][nx] = 1;
+  }
+
+  while(!q.empty()){
+    auto [pos, sc, p] = q.front();
+    auto [y, x] = pos;
+    q.pop();
+    if(pos == gl){
+      if(p == 0) return distP0[y][x];
+      else return distP1[y][x];
+    }
+    for(ll i=0;i<4;i++){
+      if(i%2==sc%2)continue;
+      ll nx = x + dx[i];
+      ll ny = y + dy[i];
+      if(!isRange(nx, ny, W, H))continue;
+      if(G[ny][nx]=='#')continue;
+      if(p==0){
+        if(distP0[ny][nx] != -1)continue;
+        distP0[ny][nx] = distP0[y][x] + 1;
+      }else{
+        if(distP1[ny][nx] != -1)continue;
+        distP1[ny][nx] = distP1[y][x] + 1;
       }
-      if(p.prev_action == 'Y' && i % 2 == 1) {
-        continue;
-      }
-      int nx = p.x + dx[i];
-      int ny = p.y + dy[i];
-      if(nx < 0 || nx >= H || ny < 0 || ny >= W) {
-        continue;
-      }
-      if(field[nx][ny] == 'G') {
-        return dist[p.x][p.y] + 1;
-      }
-      if(field[nx][ny] == '#') {
-        continue;
-      }
-      if(dist[nx][ny] != -1) {
-        continue;
-      }
-      rep(j,0,4) {
-        if(i % 2 == j % 2) {
-          continue;
-        }
-        int nnx = nx + dx[j];
-        int nny = ny + dy[j];
-        if(nnx < 0 || nnx >= H || nny < 0 || nny >= W) {
-          continue;
-        }
-        if(field[nnx][nny] == 'G') {
-          return dist[p.x][p.y] + 1;
-        }
-        if(field[nnx][nny] == '#') {
-          continue;
-        }
-        if(dist[nnx][nny] != -1) {
-          continue;
-        }
-        dist[nnx][nny] = dist[p.x][p.y] + 2;
-        if(i % 2 == 0){
-          que.push(pnt{nx, ny, 'X'});
-        }else{
-          que.push(pnt{nx, ny, 'Y'});
-        }
-      }
+      q.push({mp(ny, nx), sc + 1, p});
     }
   }
   return -1;
 }
 
-int main()
-{
+int main(){
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  cin >> H >> W;
-  vs S(H);
-  vcin(H,S);
-  rep(i,0,H) {
-    rep(j,0,W) {
-      if(S[i][j] == 'S') {
-        cout << bfs(S, i, j) << '\n';
-        return 0;
-      }
+  cin>>H>>W;
+  for(ll i=0; i<H; i++){
+    cin>>G[i];
+    for(ll j=0;j<W;j++){
+      if(G[i][j]=='S')st=mp(i,j);
+      if(G[i][j]=='G')gl=mp(i,j);
     }
   }
+  cout<<bfs()<<endl;
   return 0;
 }
