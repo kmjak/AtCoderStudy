4211dd8c80123549add70499fa954cc6264f46a4
kmjak <tymkoron1221@icloud.com>
2025-10-08 11:22:28 +0900

    ABC340: D問題をダイクストラ法で解いた

---
+++ contest/ABC/ABC340/D.cpp
@@ -0,0 +1,176 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define g(i,t) get<i>(t)
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define btoi(b) static_cast<ll>(b.to_ulong())
+
+using namespace std;
+
+using ll=long long int;
+using qll=queue<ll>;
+using dq=deque<ll>;
+
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+class SegmentTreeRMQ {
+public:
+  vll data;
+  ll size = 1;
+
+  void init(ll n){
+    while(size < n) size *= 2;
+    data.assign(size * 2, 0);
+  }
+
+  void update(ll pos, ll x){
+    pos += size - 1;
+    data[pos] = x;
+    while(pos >= 2){
+      pos /= 2;
+      data[pos] = max(data[pos*2], data[pos*2+1]);
+    }
+  }
+
+  ll query(ll l, ll r, ll bl, ll br, ll u){
+    if(r <= bl || br <= l)return -1e18;
+    if(l <= bl && br <= r)return data[u];
+    ll m = (bl + br) / 2;
+    ll al = query(l, r, bl, m, u*2);
+    ll ar= query(l, r, m, br, u*2+1);
+    return max(al, ar);
+  }
+};
+
+class SegmentTreeRSQ {
+public:
+  vll data;
+  ll size = 1;
+
+  void init(ll n){
+    while(size < n) size *= 2;
+    data.assign(size * 2, 0);
+  }
+
+  void update(ll pos, ll x){
+    pos += size - 1;
+    data[pos] = x;
+    while(pos >= 2){
+      pos /= 2;
+      data[pos] = data[pos*2] + data[pos*2+1];
+    }
+  }
+
+  ll query(ll l, ll r, ll bl, ll br, ll u){
+    if(r <= bl || br <= l)return 0;
+    if(l <= bl && br <= r)return data[u];
+    ll m = (bl + br) / 2;
+    ll al = query(l, r, bl, m, u*2);
+    ll ar= query(l, r, m, br, u*2+1);
+    return al + ar;
+  }
+};
+
+class UnionFind {
+public:
+  vll parent, size;
+
+  UnionFind(ll n) : parent(n, -1), size(n, 1) {}
+
+  ll find(ll x) {
+    if (parent[x] == -1) return x;
+    return parent[x] = find(parent[x]);
+  }
+
+  bool unite(ll a, ll b) {
+    a = find(a);
+    b = find(b);
+    if (a == b) return false;
+    if (size[a] < size[b]) swap(a, b);
+    parent[b] = a;
+    size[a] += size[b];
+    return true;
+  }
+
+  bool same(ll a, ll b) {
+    return find(a) == find(b);
+  }
+
+  ll getSize(ll x) {
+    return size[find(x)];
+  }
+};
+
+bool isRange(ll x, ll y, ll w, ll h){
+  return (0 <= x && x < w) && (0 <= y && y < h);
+}
+
+/**
+ * 考察
+ *
+ */
+
+vll dx={1, 0, -1, 0};
+vll dy={0, 1, 0, -1};
+const ll mod=1e9+7;
+const ll inf=1e18;
+vector<vector<pair<ll,ll>>> G(2e5+10);
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  ll N;cin>>N;
+  for(ll i=0;i<N-1;i++){
+    ll a,b,x;
+    cin>>a>>b>>x;
+    x--;
+    G[i].eb(i+1,a);
+    G[i].eb(x,b);
+  }
+
+  priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> pq;
+  pq.push({0,0});
+  vll dist(N,inf);
+
+  while(!pq.empty()){
+    auto [score, x]=pq.top();pq.pop();
+    // cout<<score<<endl;
+    for(auto [nx, cost]: G[x]){
+      ll ns=score+cost;
+      if(ns>=dist[nx])continue;
+      dist[nx]=ns;
+      pq.push({ns,nx});
+    }
+  }
+  cout<<dist[N-1]<<endl;
+  return 0;
+}
