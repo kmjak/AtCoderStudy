38e6198b4614a2c3b292d16e9b4382e22b5e1f4f
kmjak <tymkoron1221@icloud.com>
2026-02-08 00:05:02 +0900

    ABC444: C問題むずすぎ。E問題簡単だったから崎にとけばよかった

---
+++ contest/ABC/444/C.cpp
@@ -0,0 +1,217 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+
+#define tos(n) to_string(n)
+#define toc(n) '0' + n
+#define btoi(b) static_cast<ll>(b.to_ulong())
+
+using namespace std;
+
+using ll=long long int;
+using qll=queue<ll>;
+using dq=deque<ll>;
+
+using vll=vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using vb=vector<bool>;
+
+using vvll=vector<vll>;
+
+template<typename T> inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template<typename T> inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+class SegmentTreeRMQ {
+public:
+  vll data;
+  ll size = 1;
+
+  void init(ll n){
+    while(size < n) size *= 2;
+    data.assign(size * 2, 0);
+  }
+
+  void update(ll pos, ll x){
+    pos += size - 1;
+    data[pos] = x;
+    while(pos >= 2){
+      pos /= 2;
+      data[pos] = max(data[pos*2], data[pos*2+1]);
+    }
+  }
+
+  ll query(ll l, ll r, ll bl, ll br, ll u){
+    if(r <= bl || br <= l)return -1e18;
+    if(l <= bl && br <= r)return data[u];
+    ll m = (bl + br) / 2;
+    ll al = query(l, r, bl, m, u*2);
+    ll ar= query(l, r, m, br, u*2+1);
+    return max(al, ar);
+  }
+};
+
+class SegmentTreeRSQ {
+public:
+  vll data;
+  ll size = 1;
+
+  void init(ll n){
+    while(size < n) size *= 2;
+    data.assign(size * 2, 0);
+  }
+
+  void update(ll pos, ll x){
+    pos += size - 1;
+    data[pos] = x;
+    while(pos >= 2){
+      pos /= 2;
+      data[pos] = data[pos*2] + data[pos*2+1];
+    }
+  }
+
+  ll query(ll l, ll r, ll bl, ll br, ll u){
+    if(r <= bl || br <= l)return 0;
+    if(l <= bl && br <= r)return data[u];
+    ll m = (bl + br) / 2;
+    ll al = query(l, r, bl, m, u*2);
+    ll ar= query(l, r, m, br, u*2+1);
+    return al + ar;
+  }
+};
+
+class UnionFind {
+public:
+  vll parent, size;
+
+  UnionFind(ll n) : parent(n, -1), size(n, 1) {}
+
+  ll find(ll x) {
+    if (parent[x] == -1) return x;
+    return parent[x] = find(parent[x]);
+  }
+
+  bool unite(ll a, ll b) {
+    a = find(a);
+    b = find(b);
+    if (a == b) return false;
+    if (size[a] < size[b]) swap(a, b);
+    parent[b] = a;
+    size[a] += size[b];
+    return true;
+  }
+
+  bool same(ll a, ll b) {
+    return find(a) == find(b);
+  }
+
+  ll getSize(ll x) {
+    return size[find(x)];
+  }
+};
+
+class IntervalSet {
+  public:
+  set<pair<ll,ll>> intervals;
+  ll covered_length = 0;
+  ll infinity = 1e18;
+
+  bool merge(ll l, ll r){
+    auto it = intervals.lower_bound({l, infinity});
+    if(it!=intervals.begin()){
+      it--;
+      if(it->second<l-1)it++;
+    }
+    while(it != intervals.end() && it->first <= r + 1) {
+      chmin(l, it->first);
+      chmax(r, it->second);
+      covered_length -= (it->second - it->first + 1);
+      it=intervals.erase(it);
+    }
+    covered_length+=(r-l+1);
+    return intervals.insert({l,r}).second;
+  }
+};
+
+bool isRange(ll x, ll y, ll w, ll h){
+  return (0 <= x && x < w) && (0 <= y && y < h);
+}
+
+/**
+ * 考察
+ * むずい
+ * 答えはmax(L) or max(L)+min(L)の2パターンしかないので
+ * それに気づいたら解くだけ
+ */
+
+vll dx={1, 0, -1, 0};
+vll dy={0, 1, 0, -1};
+const ll mod=1e9+7;
+const ll inf=1e18;
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  ll N;cin>>N;
+  vll L(N);
+  for(auto &x: L)cin>>x;
+
+  sort(all(L));
+  vector<pair<ll,ll>> m;
+  for(ll l: L){
+    if(m.empty()||m.back().first!=l)m.eb(l,1LL);
+    else m.back().second++;
+  }
+
+  ll M=m.size();
+  bool ok=true;
+  ll l=0,r=M-2;
+  ll tg=m.back().first;
+  while(r-l>=0){
+    if(r==l){
+      if(m[r].second%2==1)ok=false;
+    }else{
+      if(m[l].first+m[r].first!=tg)ok=false;
+      if(m[l].second!=m[r].second)ok=false;
+    }
+    r--;l++;
+  }
+
+  if(ok)cout<<tg<<" ";
+
+  ok=true;
+  l=0,r=M-1;
+  tg=m.back().first+m.front().first;
+  while(r-l>=0){
+    if(r==l){
+      if(m[r].second%2==1)ok=false;
+    }else{
+      if(m[l].first+m[r].first!=tg)ok=false;
+      if(m[l].second!=m[r].second)ok=false;
+    }
+    r--;l++;
+  }
+
+  if(ok)cout<<tg<<" ";
+  cout<<endl;
+  return 0;
+}
